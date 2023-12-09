27d0151edf6ef2dadf2f8601f8ce29bcb8cd6e51
xmasuhai <xmasuhai@163.com>
2023-12-10 07:44:13 +0800

    添加 version.sh 查看版本号脚本

--- version.sh
+++ version.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 
-#Print current user
-whoami
+# 判断是否存在 版本
+ifHasVersionPrint() {
+  local exe="${1:?"exe name missing."}"
+  local install_info=$2
+  if command_path=$(command -v $1 2>/dev/null); then
+    echo "${exe^^} Version: $($1 --version)"
+  else
+    echo "${exe^^} is not installed"
+    if [[ -n $install_info ]]; then
+      echo "Please run: $install_info"
+    fi
+  fi
+}
 
-#Print current system
-uname -a
+# Function to print Node.js version in current path
+print_env_version() {
+  # Print separator
+  echo "-------print_env_version-------"
+  #Print current user
+  whoami
+  #Print current system
+  uname -a
+  # Print Node.js version
+  ifHasVersionPrint node;
+  # Print nvm version
+  ifHasVersionPrint nvm;
+  nvm_current=$(nvm current)
+  echo "nvm current: $nvm_current"
+  # Print nrm current
+  ifHasVersionPrint nrm 'npm install -g nrm';
+  nrm_current=$(nrm current)
+  echo "nrm current: $nrm_current"
+  # Print npm version
+  ifHasVersionPrint npm;
+  # Print pnpm version
+  ifHasVersionPrint pnpm 'corepack prepare pnpm@latest --activate';
+  # Print yarn version
+  ifHasVersionPrint yarn 'corepack prepare yarn@1.22.19 --activate';
+  # Print ruby version
+  ifHasVersionPrint ruby;
 
-echo "-----------------------------"
+  # Print go version
+  go_version=$(go version)
+  echo "go version: $go_version"
+  # Print separator
+  echo "--------------------------------"
+}
 
-# Print Node.js version
-node_version=$(node --version)
-echo "Node.js version: $node_version"
-
-# Print npm version
-npm_version=$(npm --version)
-echo "npm version: $npm_version"
-
-# Print pnpm version
-pnpm_version=$(pnpm --version)
-echo "pnpm version: $pnpm_version"
-
-# Print yarn version
-yarn_version=$(yarn --version)
-echo "yarn version: $yarn_version"
-
-echo "-----------------------------"
-
-nrm_current=$(nrm current)
-echo "nrm current: $nrm_current"
-
-echo "-----------------------------"
-
-# Print ruby version
-ruby_version=$(ruby --version)
-echo "ruby version: $ruby_version"
-
-# Print go version
-go_version=$(go version)
-echo "go version: $go_version"
+if [[ -n "$BASH_VERSION" ]]; then
+  print_env_version
+fi
